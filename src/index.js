85dd16c3802442b64907f2eecd0a609d81afc164
optimized faisal <optimizedfaisal42@gmail.com>
2019-03-15 16:37:32 +0600

    Chap 2 completed

--- src/index.js
+++ src/index.js
@@ -1,12 +1,24 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+//import the react and ReactDom libraries
+import React from "react"; //React hocce variable er name.. react dependancy theke sob kusi React var e store hocce
+import ReactDOM from "react-dom";
 
-ReactDOM.render(<App />, document.getElementById('root'));
+//create a react component
+const App = () => {
+  let clickObj = { text: "click me" };
+  let labelText = "Enter name : ";
+  return (
+    <div>
+      <label class="label" htmlFor="name">
+        {labelText}
+      </label>
+      <input id="name" type="text" />
+      <button style={{ backgroundColor: "blue", color: "white" }}>
+        {clickObj.text}
+      </button>
+    </div>
+  );
+};
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+//Take the react component and show it on the screen
+
+ReactDOM.render(<App />, document.querySelector("#root"));
